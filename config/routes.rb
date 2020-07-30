de1242be157fd3f232c2677c473ace3f1d6065ff
Alan <alanmannon@gmail.com>
2020-07-29 21:18:14 -0500

    created initial routes and users/user_groups controllers

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,16 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/user_group" => "user_group#index"
+    post "/user_group" => "user_group#create"
+    get "/user_group/:id" => "user_group#show"
+    patch "/user_group/:id" => "user_group#update"
+    delete "/user_group/:id" => "user_group#destroy"
+
+    post "/user" => "user#create"
+    get "/user/:id" => "user#show"
+    patch "/user/:id" => "user#update"
+  end
 end
